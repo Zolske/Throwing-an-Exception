1bd6c2b63b871482790b7fa078810bf45cb68901
Matt Sturgeon <matt@sturgeon.me.uk>
2024-10-30 11:15:26 +0000

    feat(ex1): initial divideUserInputs method
    Added divideUserInputs and getUserInputs methods to Excersise1.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,7 @@ import exercises.Exercise1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Exercise1.divide(10, 2));
-        System.out.println(Exercise1.divide(10, 0));
+        Exercise1.divideUserInputs();
     }
 
 }
--- src/main/java/exercises/Exercise1.java
+++ src/main/java/exercises/Exercise1.java
@@ -1,7 +1,41 @@
 package exercises;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Exercise1 {
     public static double divide(int numerator, int dominator) {
         return numerator / dominator;
+    }
+
+    public static void divideUserInputs(){
+        int[] numbers;
+        while (true) {
+            // Try and get numbers array from getUserInput()
+            try {
+                numbers = getUserInputs();
+            } catch (InputMismatchException e) {
+                // Keep trying until we have valid input
+                System.out.println("Invalid input, please try again.");
+                continue;
+            }
+            // If we have valid numbers and no exception was thrown,
+            // we can terminate the loop
+            break;
+        }
+
+        // Get numerator from numbers[0]
+        // Get denominator from numbers[1]
+        // Try and get result from divide()
+        //     if successful, print result to the console
+    }
+
+    private static int[] getUserInputs() throws InputMismatchException {
+        var scanner = new Scanner(System.in);
+        System.out.print("Enter divisor: ");
+        int divisor = scanner.nextInt();
+        System.out.print("Enter dividend: ");
+        int dividend = scanner.nextInt();
+        return new int[]{divisor, dividend};
     }
 }
